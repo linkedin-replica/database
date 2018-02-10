67d594549f7161e29d006b3ec0bb47db6579d54b
Rana Saeed <ranas.2122@gmail.com>
2018-02-10 16:55:59 +0200

    Testing stored procedures + added search_user_by_skill procedure + merging conflicts

--- sql/Procedures.sql
+++ sql/Procedures.sql
@@ -22,11 +22,20 @@ select * from jobs where title=title_search;
 END$$
 DELIMITER ;
 
+DROP procedure IF EXISTS `search_for_user_by_skill`;
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `search_for_user_by_skill`(skill_id_in int)
+BEGIN
+select * from user_has_skill where skill_id=skill_id_in;
+END$$
+DELIMITER ;
+
+
 DROP procedure IF EXISTS `view_applied_jobs`;
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `view_applied_jobs`(id int)
 BEGIN
-select * from user_applies_for_job where user_id=id;
+select * from user_applied_for_job where user_id=id;
 END$$
 DELIMITER ;
 
@@ -40,75 +49,74 @@ DELIMITER ;
 
 DROP PROCEDURE IF EXISTS `delete_company`;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_company`(id int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_company`(id_company int)
 BEGIN
-DELETE FROM companies WHERE id = id;
-
+DELETE FROM companies WHERE id = id_company;
 END$$
 DELIMITER ;
 
 DROP PROCEDURE IF EXISTS `delete_job`;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_job`(id int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_job`(id_job int)
 BEGIN
-DELETE FROM jobs WHERE id =id;
+DELETE FROM jobs WHERE id =id_job;
 END$$
 DELIMITER ;
 
 DROP PROCEDURE IF EXISTS `delete_job_needs_skill`;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_job_needs_skill`(job_id int, skill_id int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_job_needs_skill`(job_id_in int, skill_id_in int)
 BEGIN
-DELETE FROM job_needs_skill WHERE job_id = job_id AND skill_id = skill_id;
+DELETE FROM job_requires_skill WHERE job_id = job_id_in AND skill_id = skill_id_in;
 END$$
 DELIMITER ;
 
 DROP PROCEDURE IF EXISTS `delete_skill`;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_skill`(id int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_skill`(id_skill int)
 BEGIN
-DELETE FROM skills WHERE id = id;
+DELETE FROM skills WHERE id = id_skill;
 END$$
 DELIMITER ;
 
 DROP PROCEDURE IF EXISTS `delete_user`;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user`(id int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user`(id_user int)
 BEGIN
-DELETE FROM users WHERE id = id;
+DELETE FROM users WHERE id = id_user;
 END$$
 DELIMITER ;
 
 DROP PROCEDURE IF EXISTS `delete_user_apply_for_job`;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user_apply_for_job`(user_id int,  job_id int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user_apply_for_job`(user_id_in int,  job_id_in int)
 BEGIN
-DELETE FROM user_apply_for_job  WHERE user_id = user_id AND job_id = job_id;
+DELETE FROM user_applied_for_job  WHERE user_id = user_id_in AND job_id = job_id_in;
 END$$
 DELIMITER ;
 
 
 DROP PROCEDURE IF EXISTS `delete_user_blocks_user`;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user_blocks_user`(blocking_user_id int, blocked_user_id int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user_blocks_user`(blocking_user_id_in int, blocked_user_id_in int)
 BEGIN
-DELETE FROM user_blocks_user WHERE blocking_user_id = blocking_user_id AND blocked_user_id;
+DELETE FROM user_blocked_user WHERE blocking_user_id = blocking_user_id_in AND blocked_user_id = blocked_user_id_in;
 END$$
 DELIMITER ;
 
 DROP PROCEDURE IF EXISTS `delete_user_friend_with_user`;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user_friend_with_user`(user1_id int, user2_id int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user_friend_with_user`(user1_id_in int, user2_id_in int)
 BEGIN
-DELETE FROM user_friend_with_user WHERE user1_id = user1_id AND user2_id =user2_id;
+DELETE FROM user_friends_with_user WHERE user1_id = user1_id_in AND user2_id =user2_id_in;
 END$$
 DELIMITER ;
 
 DROP PROCEDURE IF EXISTS `delete_user_has_skill`;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user_has_skill`(user_id int, skill_id int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_user_has_skill`(user_id_in int, skill_id_in int)
 BEGIN
-DELETE FROM user_has_skil WHERE user_id = user_id AND skill_id = skill_id;
+DELETE FROM user_has_skill WHERE user_id = user_id_in AND skill_id = skill_id_in;
 END$$
 DELIMITER ;
 
