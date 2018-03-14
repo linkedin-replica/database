e7d55b1d03f41dbeda6b07af241523de71c2eb84
ahmed-anwar <ahmedanwarm@gmail.com>
2018-03-14 15:57:50 +0200

    fixed deletion stored procedures

--- sql/StoredProcedures/DeletionStoredProcedures.sql
+++ sql/StoredProcedures/DeletionStoredProcedures.sql
@@ -1,7 +1,7 @@
 DROP PROCEDURE IF EXISTS `delete_company`;
 
 DELIMITER $$
-CREATE PROCEDURE `delete_company`(id_company varchar(25))
+CREATE PROCEDURE `delete_company`(id_company varchar(40))
   BEGIN
     DELETE FROM companies WHERE id = id_company;
   END$$
@@ -11,7 +11,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS `delete_job`;
 
 DELIMITER $$
-CREATE PROCEDURE `delete_job`(id_job varchar(25))
+CREATE PROCEDURE `delete_job`(id_job varchar(40))
   BEGIN
     DELETE FROM jobs WHERE id = id_job;
   END$$
@@ -21,7 +21,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS `delete_job_needs_skill`;
 
 DELIMITER $$
-CREATE PROCEDURE `delete_job_needs_skill`(job_id_in varchar(25), skill_id_in varchar(25))
+CREATE PROCEDURE `delete_job_needs_skill`(job_id_in varchar(40), skill_id_in varchar(40))
   BEGIN
     DELETE FROM job_requires_skill WHERE job_id = job_id_in AND skill_id = skill_id_in;
   END$$
@@ -31,7 +31,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS `delete_skill`;
 
 DELIMITER $$
-CREATE PROCEDURE `delete_skill`(id_skill varchar(25))
+CREATE PROCEDURE `delete_skill`(id_skill varchar(40))
   BEGIN
     DELETE FROM skills WHERE id = id_skill;
   END$$
@@ -41,7 +41,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS `delete_user`;
 
 DELIMITER $$
-CREATE PROCEDURE `delete_user`(id_user varchar(25))
+CREATE PROCEDURE `delete_user`(id_user varchar(40))
   BEGIN
     DELETE FROM users WHERE id = id_user;
   END$$
@@ -50,7 +50,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS `delete_user_apply_for_job`;
 
 DELIMITER $$
-CREATE PROCEDURE `delete_user_apply_for_job`(user_id_in varchar(25),  job_id_in varchar(25))
+CREATE PROCEDURE `delete_user_apply_for_job`(user_id_in varchar(40),  job_id_in varchar(40))
   BEGIN
     DELETE FROM user_applied_for_job  WHERE user_id = user_id_in AND job_id = job_id_in;
   END$$
@@ -60,7 +60,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS `delete_user_blocks_user`;
 
 DELIMITER $$
-CREATE PROCEDURE `delete_user_blocks_user`(blocking_user_id_in varchar(25), blocked_user_id_in varchar(25))
+CREATE PROCEDURE `delete_user_blocks_user`(blocking_user_id_in varchar(40), blocked_user_id_in varchar(40))
   BEGIN
     DELETE FROM user_blocked_user WHERE blocking_user_id = blocking_user_id_in AND blocked_user_id = blocked_user_id_in;
   END$$
@@ -70,7 +70,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS `delete_user_friend_with_user`;
 
 DELIMITER $$
-CREATE PROCEDURE `delete_user_friend_with_user`(user1_id_in varchar(25), user2_id_in varchar(25))
+CREATE PROCEDURE `delete_user_friend_with_user`(user1_id_in varchar(40), user2_id_in varchar(40))
   BEGIN
     DELETE FROM user_friends_with_user WHERE user1_id = user1_id_in AND user2_id = user2_id_in;
   END$$
@@ -80,7 +80,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS `delete_user_has_skill`;
 
 DELIMITER $$
-CREATE PROCEDURE `delete_user_has_skill`(user_id_in varchar(25), skill_id_in varchar(25))
+CREATE PROCEDURE `delete_user_has_skill`(user_id_in varchar(40), skill_id_in varchar(40))
   BEGIN
     DELETE FROM user_has_skill WHERE user_id = user_id_in AND skill_id = skill_id_in;
   END$$
