a81d2738576017e0985aa7a09ea32feb90efe47e
ahmed-anwar <ahmedanwarm@gmail.com>
2018-03-14 12:03:10 +0200

    changed ids type to strings

--- sql/DDL/CreateScript.sql
+++ sql/DDL/CreateScript.sql
@@ -6,35 +6,35 @@ USE linkedin;
 
 -- create users table
 CREATE TABLE IF NOT EXISTS users(
-  id int  AUTO_INCREMENT PRIMARY KEY,
+  id varchar(25) PRIMARY KEY,
   email varchar(50) UNIQUE,
   password  varchar(512)
 );
 
 
 CREATE TABLE IF NOT EXISTS companies(
-  id int AUTO_INCREMENT PRIMARY KEY,
+  id varchar(25) PRIMARY KEY,
   name varchar(50) UNIQUE,
-  user_id int NOT NULL UNIQUE,
+  user_id varchar(25) NOT NULL UNIQUE,
   FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS skills(
-  id int AUTO_INCREMENT PRIMARY KEY,
+  id varchar(25) PRIMARY KEY,
   name varchar(50),
   type varchar(50)
 );
 
 CREATE TABLE IF NOT EXISTS jobs(
-  id int AUTO_INCREMENT PRIMARY KEY,
+  id varchar(25) PRIMARY KEY,
   title varchar(50),
-  company_id int NOT NULL,
+  company_id varchar(25) NOT NULL,
   FOREIGN KEY (company_id) REFERENCES companies(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS user_has_skill(
-  user_id int NOT NULL,
-  skill_id int NOT NULL,
+  user_id varchar(25) NOT NULL,
+  skill_id varchar(25) NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE ,
   FOREIGN KEY (skill_id) REFERENCES skills(id) ON UPDATE CASCADE ON DELETE CASCADE ,
 
@@ -42,8 +42,8 @@ CREATE TABLE IF NOT EXISTS user_has_skill(
 );
 
 CREATE TABLE IF NOT EXISTS job_requires_skill(
-  job_id int NOT NULL,
-  skill_id int NOT NULL,
+  job_id varchar(25) NOT NULL,
+  skill_id varchar(25) NOT NULL,
   FOREIGN KEY (job_id) REFERENCES jobs(id) ON UPDATE CASCADE ON DELETE CASCADE ,
   FOREIGN KEY (skill_id) REFERENCES skills(id) ON UPDATE CASCADE ON DELETE CASCADE ,
 
@@ -52,8 +52,8 @@ CREATE TABLE IF NOT EXISTS job_requires_skill(
 
 
 CREATE TABLE IF NOT EXISTS user_applied_for_job(
-  user_id int,
-  job_id int,
+  user_id varchar(25),
+  job_id varchar(25),
   status tinyint,
 
   FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE ,
@@ -64,8 +64,8 @@ CREATE TABLE IF NOT EXISTS user_applied_for_job(
 
 
 CREATE TABLE IF NOT EXISTS user_saved_job(
-  user_id int,
-  job_id int,
+  user_id varchar(25),
+  job_id varchar(25),
 
   FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE ,
   FOREIGN KEY (job_id) REFERENCES jobs(id) ON UPDATE CASCADE ON DELETE CASCADE ,
@@ -74,8 +74,8 @@ CREATE TABLE IF NOT EXISTS user_saved_job(
 );
 
 CREATE TABLE IF NOT EXISTS user_blocked_user(
-  blocking_user_id int,
-  blocked_user_id int,
+  blocking_user_id varchar(25),
+  blocked_user_id varchar(25),
 
   FOREIGN KEY (blocking_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE ,
   FOREIGN KEY (blocked_user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE ,
@@ -85,8 +85,8 @@ CREATE TABLE IF NOT EXISTS user_blocked_user(
 
 -- is_accepted value 0 means user1 added user2, value 1 means user2 added user1, and value 2 means user1 and user2 are friends
 CREATE TABLE IF NOT EXISTS user_friends_with_user(
-  user1_id int,
-  user2_id int,
+  user1_id varchar(25),
+  user2_id varchar(25),
   is_accepted int,
 
   FOREIGN KEY (user1_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE ,
--- sql/StoredProcedures/UpdateStoredProcedures.sql
+++ sql/StoredProcedures/UpdateStoredProcedures.sql
@@ -1,7 +1,7 @@
 DROP PROCEDURE IF EXISTS `edit_password`;
 
 DELIMITER $$
-CREATE PROCEDURE `edit_password`( user_id int,  new_password varchar(512))
+CREATE PROCEDURE `edit_password`( user_id varchar(25),  new_password varchar(512))
   BEGIN
     UPDATE users SET password = new_password WHERE id = user_id;
   END$$
@@ -10,7 +10,7 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS `accept_friend_request`;
 
 DELIMITER $$
-CREATE PROCEDURE `accept_friend_request`( user_id1 int, user_id2 int)
+CREATE PROCEDURE `accept_friend_request`( user_id1 varchar(25), user_id2 varchar(25))
   BEGIN
     UPDATE user_friends_with_user SET is_accepted = 2 WHERE user1_id = user_id1 AND user2_id = user_id2;
   END$$
