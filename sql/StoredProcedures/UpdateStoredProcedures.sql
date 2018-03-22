03f8600bab4bc150b99d7a113ef0cbca06871d83
BaherAbdelmalek <baher.abdelmalek100@gmail.com>
2018-03-22 20:57:36 +0200

    respond to applicant stored procedure created

--- sql/StoredProcedures/UpdateStoredProcedures.sql
+++ sql/StoredProcedures/UpdateStoredProcedures.sql
@@ -6,3 +6,12 @@ CREATE PROCEDURE `edit_password`( user_id int,  new_password varchar(512))
     UPDATE users SET password = new_password WHERE id = user_id;
   END$$
 DELIMITER ;
+
+
+DROP procedure IF EXISTS `respond_to_applicant`;
+DELIMITER $$
+    CREATE PROCEDURE `respond_to_applicant`( User_id int,  Job_id int, new_status int)
+           BEGIN
+        UPDATE user_applied_for_job j SET j.status = new_status WHERE j.user_id = User_id AND j.job_id = Job_id;
+     END$$
+DELIMITER ;
