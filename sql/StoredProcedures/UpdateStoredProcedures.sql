a7da0c9509c1efc74753e685cd6278fc70689f58
ahmed-anwar <ahmedanwarm@gmail.com>
2018-03-23 22:09:32 +0200

    added respont to applicant SP and updated insert job SP

--- sql/StoredProcedures/UpdateStoredProcedures.sql
+++ sql/StoredProcedures/UpdateStoredProcedures.sql
@@ -15,3 +15,11 @@ CREATE PROCEDURE `accept_friend_request`( user_id1 varchar(40), user_id2 varchar
     UPDATE user_friends_with_user SET is_accepted = 2 WHERE user1_id = user_id1 AND user2_id = user_id2;
   END$$
 DELIMITER ;
+
+DROP procedure IF EXISTS `respond_to_applicant`;
+DELIMITER $$
+    CREATE PROCEDURE `respond_to_applicant`( User_id int,  Job_id int, new_status int)
+           BEGIN
+        UPDATE user_applied_for_job j SET j.status = new_status WHERE j.user_id = User_id AND j.job_id = Job_id;
+     END$$
+DELIMITER ;
